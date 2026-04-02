211bf56a2457b7f9199442c087889b86eed47ff6
Guilhem Fauré <pro@guilhemfau.re>
2026-04-02 20:05:07 +0200

    build: android prerelease every hour

--- .script/prerelease.sh
+++ .script/prerelease.sh
@@ -2,13 +2,13 @@
 # Eventually publish timestamped pre-release
 set -e
 
-NOW=$(date -u +"%Y-%m-%d")
+NOW=$(date -u +"%Y-%m-%d-%H")
 
 CREATED_AT=$(gh release list --json publishedAt,isPrerelease \
   --jq '.[] | select(.isPrerelease == true) | .publishedAt' | head -n 1)
 
 if [ -z "$CREATED_AT" ] ||
-  [ "$(date -d "$CREATED_AT" +%s)" -le "$(date -d '2 days ago' +%s)" ]; then
+  [ "$(date -d "$CREATED_AT" +%s)" -le "$(date -d 'a hour ago' +%s)" ]; then
   gh release create "pre-$NOW" *.apk --prerelease \
     --title "$NOW" --notes "Automated pre-release for $NOW"
 fi
