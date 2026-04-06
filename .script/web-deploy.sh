4acc71673a5837189e450f2d5953daa871b594a0
Guilhem Fauré <pro@guilhemfau.re>
2026-04-06 02:24:51 +0200

    build: fix preview path

--- .script/web-deploy.sh
+++ .script/web-deploy.sh
@@ -4,4 +4,4 @@ cp --dereference --recursive -v relWeb/LogOut web/LogOut
 chmod --recursive 775 web # Files from Nix store come read-only
 tar --create --directory=web --file=web.tar.gz --gzip --verbose LogOut
 nix build .#preWeb --out-link preWeb # Set preview to this Release
-cp --dereference --recursive -v preWeb/LogOut web/LogOut/preview
+cp --dereference --recursive -v preWeb/LogOut/preview web/LogOut/preview
